c9b504faebbc1ae01aa108338b6ffab7f0198a9c
Dwyriel <dwyriel333@hotmail.com>
2021-04-16 17:22:43 -0300

    Firebase Installation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBnNctz0o2WRE4HLLgx2boA--LQ_mPerb0",
+    authDomain: "projeto-modulo6.firebaseapp.com",
+    projectId: "projeto-modulo6",
+    storageBucket: "projeto-modulo6.appspot.com",
+    messagingSenderId: "376359042175",
+    appId: "1:376359042175:web:f716a51a9126c64069934a"
+  }
 };
