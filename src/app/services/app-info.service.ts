2a77b095f1e5f7984d5790fa1e29a6c0f370ec65
Dwyriel <dwyriel333@hotmail.com>
2021-04-19 21:00:46 -0300

    AppInfo Service

---
+++ src/app/services/app-info.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { User } from '../structure/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppInfoService {
+
+  private static appInfo = new BehaviorSubject<{ appWidth: number, appHeight: number, isDesktop: boolean }>({ appWidth: null, appHeight: null, isDesktop: null });
+  private static userInfo = new BehaviorSubject<User>(new User());
+
+  /**
+   * Puts and pushes new information into appInfo to all listeners.
+   * @param object the new information for appInfo.
+   */
+  public static PushAppInfo(object: { appWidth: number, appHeight: number, isDesktop: boolean }) {
+    AppInfoService.appInfo.next(object);
+  }
+
+  /**
+   * Retrives the appInfo's observable.
+   * @returns an observable for the appInfo.
+   */
+  static GetAppInfo() {
+    return AppInfoService.appInfo.asObservable();
+  }
+
+  /**
+   * Puts and pushes new information into userInfo to all listeners.
+   * @param object the new information for userInfo.
+   */
+  public static PushUserInfo(object: User) {
+    AppInfoService.userInfo.next(object);
+  }
+
+  /**
+   * Retrives the userInfo's observable.
+   * @returns an observable for the userInfo.
+   */
+  static GetUserInfo() {
+    return AppInfoService.userInfo.asObservable();
+  }
+
+  constructor() { }
+}
