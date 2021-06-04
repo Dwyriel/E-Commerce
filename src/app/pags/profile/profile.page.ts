867709a2df5c70f7cee9736a87bf374f5cf892ec
Dwyriel <dwyriel333@hotmail.com>
2021-06-04 18:15:32 -0300

    Fixed Profile Subscriptions & Added Profile Ratings

--- src/app/pags/profile/profile.page.ts
+++ src/app/pags/profile/profile.page.ts
@@ -5,6 +5,7 @@ import { AddresService } from 'src/app/services/addres.service';
 import { AlertService } from 'src/app/services/alert.service';
 import { AppResources } from 'src/app/services/app-info.service';
 import { ImageService } from 'src/app/services/image.service';
+import { RatingService } from 'src/app/services/rating.service';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/structure/user';
 
@@ -29,9 +30,11 @@ export class ProfilePage implements OnInit {
   private subscription3: Subscription;
   private subscription4: Subscription;
   private subscription5: Subscription;
+  private subscription6: Subscription;
+  private subscription7: Subscription;
 
   constructor(private activatedRoute: ActivatedRoute, private userService: UserService, private imageService: ImageService,
-    private router: Router, private alertService: AlertService, private addressService: AddresService) { }
+    private router: Router, private alertService: AlertService, private addressService: AddresService, private ratingService: RatingService) { }
 
   ngOnInit() { }
 
@@ -56,6 +59,10 @@ export class ProfilePage implements OnInit {
       this.subscription4.unsubscribe();
     if (this.subscription5 && !this.subscription5.closed)
       this.subscription5.unsubscribe();
+    if (this.subscription6 && !this.subscription6.closed)
+      this.subscription6.unsubscribe();
+    if (this.subscription7 && !this.subscription7.closed)
+      this.subscription7.unsubscribe();
   }
 
   GetPlataformInfo() {
@@ -69,25 +76,33 @@ export class ProfilePage implements OnInit {
     document.body.style.setProperty('--maxWidth', value);
   }
 
-  async getUser() {
+  async getUser() {//this method can and should be divided into multiple methods
     var shouldWait: boolean = true;
+    var shouldWait2: boolean = true;
     await this.alertService.presentLoading().then(ans => this.loadingPopupID = ans);
     this.id = this.activatedRoute.snapshot.paramMap.get("id");
     if (this.id) {
       this.subscription1 = (await this.userService.Get(this.id)).subscribe(async ans => {
         this.user = ans;
+        this.user.calculateAvgRating = new User().calculateAvgRating;
         this.telephone = this.GetUITel(ans.tel);
         this.title = `Perfil de ${ans.name}`;
-        if (ans.addressId)
-          this.subscription2 = (await this.addressService.Get(ans.addressId)).subscribe(async ans2 => { this.user.address = ans2; shouldWait = false; });
+        if (ans.addressId) {
+          if (this.subscription2 && !this.subscription2.closed)
+            this.subscription2.unsubscribe();
+          this.subscription2 = (await this.addressService.Get(ans.addressId)).subscribe(async ans2 => { this.user.address = ans2; shouldWait = false; }, err => shouldWait = false);
+        }
         else
           shouldWait = false;
+        if (this.subscription6 && !this.subscription6.closed)
+          this.subscription6.unsubscribe();
+        this.subscription6 = (await this.ratingService.GetAllFromUser(this.id)).subscribe(ans3 => { this.user.ratings = ans3; this.user.calculateAvgRating(); shouldWait2 = false; }, err => shouldWait2 = false);
       }, async err => {
         shouldWait = false;
       });
     }
     if (this.id)
-      while (shouldWait)
+      while (shouldWait && shouldWait2)
         await new Promise(resolve => setTimeout(resolve, 10))
     this.subscription3 = AppResources.GetUserInfo().subscribe(async ans => {
       if (ans) {
@@ -95,10 +110,17 @@ export class ProfilePage implements OnInit {
         this.loggedUser.id = ans.id;
         if (!this.id) {
           this.user = ans;
+          this.user.calculateAvgRating = new User().calculateAvgRating;
           this.title = `Perfil de ${ans.name}`;
           this.telephone = this.GetUITel(ans.tel);
-          if (ans.addressId)
+          if (ans.addressId) {
+            if (this.subscription4 && !this.subscription4.closed)
+              this.subscription4.unsubscribe();
             this.subscription4 = (await this.addressService.Get(ans.addressId)).subscribe(ans2 => this.user.address = ans2);
+          }
+          if (this.subscription7 && !this.subscription7.closed)
+            this.subscription7.unsubscribe();
+          this.subscription7 = (await this.ratingService.GetAllFromUser(ans.id)).subscribe(ans3 => { this.user.ratings = ans3; this.user.calculateAvgRating(); });
         }
         await this.alertService.dismissLoading(this.loadingPopupID);
       }
